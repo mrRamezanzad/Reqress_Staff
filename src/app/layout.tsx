941f5234e39ddc79a8a4f743041091f40852b0e5
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-06-02 14:10:40 +0330

    refactor: add bootstrap css in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.css'
-import './bootstrap.min.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
