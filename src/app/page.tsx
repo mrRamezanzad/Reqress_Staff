2c9336b8a9d1c5ac527e7cf3ee9a1ccfa6d9581c
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-06-02 12:42:38 +0330

    feat(nav): replace html anchor tags with next/Link component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-
-import Script from 'next/script';
 import Nav from '@/components/nav/Nav';
 import Sort from '@/components/sort/Sort';
 import Modal from '@/components/modal/Modal';
@@ -8,15 +6,12 @@ import Main from '@/components/main/Main';
 import NewUserButton from '@/components/newUserButton/NewUserButton';
 
 export default function Home() {
-    return (
-
-        <>
-            <Nav />
-            <Sort />
-            <Main />
-            <NewUserButton />
-            <Modal />
-            <Pagination />
-        </>
-    );
+    return (<>
+        <Nav />
+        <Sort />
+        <Main />
+        <NewUserButton />
+        <Modal />
+        <Pagination />
+    </>);
 }
--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,41 +1,43 @@
+import Link from 'next/link';
+
 export default function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light mb-5 rounded-bottom sticky-top shadow-sm">
             <div className="container-fluid">
-                <a className="navbar-brand" href="">Reqress Staff</a>
+                <Link className="navbar-brand" href="">Reqress Staff</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link className="nav-link active" aria-current="page" href="#">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
+                            <Link className="nav-link" href="#">Link</Link>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <Link className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Dropdown
-                            </a>
+                            </Link>
                             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><Link className="dropdown-item" href="#">Action</Link></li>
+                                <li><Link className="dropdown-item" href="#">Another action</Link></li>
                                 <li>
                                     <hr className="dropdown-divider" />
                                 </li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                <li><Link className="dropdown-item" href="#">Something else here</Link></li>
                             </ul>
                         </li>
                         <li className="nav-item">
                             {/* fix: add tabindex */}
-                            <a id="signup" className="nav-link text-danger" href="#" data-bs-toggle="modal" data-bs-target="#more-info-modal">sign up</a>
+                            <Link id="signup" className="nav-link text-danger" href="#" data-bs-toggle="modal" data-bs-target="#more-info-modal">sign up</Link>
                             {/* <a id="signup" className="nav-link text-danger" href="#" tabindex="-1" data-bs-toggle="modal" data-bs-target="#more-info-modal">sign up</a> */}
                         </li>
                     </ul>
                     <form className="d-flex">
                         <input id="search-input" className="form-control me-2 " type="search" placeholder="Press / to Search " aria-label="Search" />
-                        <button id="search-button" className="btn btn-outline-success">Search</button>
+                        <Link href="" type="button" id="search-button" className="btn btn-outline-success">Search</Link>
                     </form>
                 </div>
             </div>
