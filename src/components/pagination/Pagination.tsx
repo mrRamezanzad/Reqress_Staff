671497c2cf65d8f47505b5bf84cf27801423f92d
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-06-02 14:25:59 +0330

    feat(pagination): use next/Link instead of anchor

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,14 +1,16 @@
+import Link from 'next/link';
+
 export default function Pagination() {
   return (
     <div className="row ">
       <nav aria-label="..." className="d-flex justify-content-center">
         <ul id="pagination" className="pagination pagination-sm">
           <li className="page-item active">
-            <a id="1" className="page-link" href="?page=1&quot;">1</a>
+            <Link id="1" className="page-link" href="?page=1&quot;">1</Link>
           </li>
 
           <li className="page-item ">
-            <a id="2" className="page-link" href=" ?page=2&quot;">2</a>
+            <Link id="2" className="page-link" href=" ?page=2&quot;">2</Link>
           </li>
         </ul>
       </nav>
