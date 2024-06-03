b3cc0370cd9adca9747a2eb9f31ea54d067421ab
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-06-03 15:17:10 +0330

    feat(main): render users from parent component state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,49 +15,53 @@ export default function Home() {
             "first_name": "ali",
             "last_name": "ahmadi",
             "email": "aliahmadi@gmail.com",
-            "avatar": "public/assets/user1.jpg"
+            "avatar": "/assets/user1.jpg"
         }, {
             id: 2,
             "first_name": "saeed",
             "last_name": "tehran",
             "email": "saeedstone@gmail.com",
-            "avatar": "public/assets/user2.jpg"
+            "avatar": "/assets/user2.jpg"
         }, {
             id: 3,
             "first_name": "saman",
             "last_name": "ashari",
             "email": "sameasghari@gmail.com",
-            "avatar": "public/assets/user6.jpg"
+            "avatar": "/assets/user6.jpg"
         }, {
             id: 4,
             "first_name": "yusef",
             "last_name": "najat",
             "email": "imyusef@gmail.com",
-            "avatar": "public/assets/user3.jpg"
+            "avatar": "/assets/user3.jpg"
         }, {
             id: 5,
             "first_name": "sami",
             "last_name": "beigi",
             "email": "avamusicsamibeigi@gmail.com",
-            "avatar": "public/assets/user4.jpg"
+            "avatar": "/assets/user4.jpg"
         }, {
             id: 6,
             "first_name": "sami",
             "last_name": "beigi",
             "email": "avamusicsamibeigi@gmail.com",
-            "avatar": "public/assets/user6.jpg"
+            "avatar": "/assets/user6.jpg"
         }, {
             id: 7,
             "first_name": "sami",
             "last_name": "beigi",
             "email": "avamusicsamibeigi@gmail.com",
-            "avatar": "public/assets/user7.png"
+            "avatar": "/assets/user7.png"
         },
     ])
+    useEffect(() => {
+        // async () => {
+    }, [users])
 
     const addNewUser = (user: User) => {
         setUsers([...users, user])
     }
+
     return (<>
         <Nav />
         <Sort />
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,20 +1,25 @@
-import Image from 'next/image';
 import Card from '../card/Card';
 
-export default function Main() {
-  const data = [
-    { id: '1', email: 'aliahmadi@gmail.com', image: '/assets/user1.jpg' },
-    { id: '2', email: 'saeedstone@gmail.com', image: '/assets/user2.jpg' },
-    { id: '3', email: 'sameasghari@gmail.com', image: '/assets/user6.jpg' },
-    { id: '4', email: 'imyusef@gmail.com', image: '/assets/user3.jpg' },
-    { id: '5', email: 'avamusicsamibeigi@gmail.com', image: '/assets/user4.jpg' },
-    { id: '6', email: 'avamusicsamibeigi@gmail.com', image: '/assets/user6.jpg' },
-  ]
+export interface User {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  avatar: string;
+}
+
+export interface MainProps {
+  users: Array<User>
+}
+
+export default function Main({ users }: MainProps) {
   return (
     <main className="row">
-      {data.map(item => {
-        return <Card key={item.id} id={item.id} email={item.email} image={item.image} />
-      })}
+      {
+        users.map(item => {
+          return <Card key={item.id} id={item.id.toString()} email={item.email} image={item.avatar} />
+        })
+      }
     </main>
   )
 }
