2d7faed376d5a585fc2725b3a638248e394913c0
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-06-03 15:07:21 +0330

    feat(update-modal): separate  UpdateModal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Modal from '@/components/modal/Modal';
 import Pagination from '@/components/pagination/Pagination';
 import Main from '@/components/main/Main';
 import NewUserButton from '@/components/newUserButton/NewUserButton';
+import UpdateModal from '@/components/update-modal/UpdateModal';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
     return (<>
@@ -11,7 +13,6 @@ export default function Home() {
         <Sort />
         <Main />
         <NewUserButton />
-        <Modal />
-        <Pagination />
+        <UpdateModal />
     </>);
 }
