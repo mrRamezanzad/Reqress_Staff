f35054519941f8f576dbbf6f149a214737acde55
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-06-03 15:28:37 +0330

    fix(new-user-button): modify to open correct signup modal

--- src/components/newUserButton/NewUserButton.tsx
+++ src/components/newUserButton/NewUserButton.tsx
@@ -3,7 +3,7 @@ import './NewUserButton.css'
 export default function NewUserButton() {
   return (
     <div className="new-user d-flex justify-content-end">
-      <button id="add-user-button" type="button" className="btn btn-dark btn-add" data-bs-toggle="modal" data-bs-target="#more-info-modal">
+      <button id="add-user-button" type="button" className="btn btn-dark btn-add" data-bs-toggle="modal" data-bs-target="#signup-modal">
         Add New Employee
       </button>
     </div>)
