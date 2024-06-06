87386e33dcf4bd9ee8af41379590a9882b379a4e
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-06-06 15:49:46 +0330

    fix(card): open correct modals

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -8,18 +8,18 @@ export interface CardProps {
 }
 
 export default function Card({ id, email, image, onClick }: CardProps) {
-  const onClickHandler = (event: React.MouseEvent<HTMLElement>): void => {
+  const onClickHandler = (): void => {
     onClick(id);
   }
 
   return (
     <div className="col-md-4 mb-3 ">
       <div className="card shadow">
-        <Image src={image} className="card-img-top rounded-circle" alt="..." width={200} height={200} data-bs-toggle="modal" data-bs-target="#update-modal" onClick={onClickHandler} />
+        <Image src={image} className="card-img-top rounded-circle" alt="..." width={200} height={200} data-bs-toggle="modal" data-bs-target="#user-detail-modal" onClick={onClickHandler} />
         <div className="card-body">
           <p className="card-text fw-bold">id: <span>{id}</span></p>
           <p className="card-text">email: <span>{email}</span></p>
-          <a user-id="1" data-bs-toggle="modal" data-bs-target="#update-modal" className="btn btn-dark main--cards--card--card-body--more-info" onClick={onClickHandler}>more info</a>
+          <a user-id="1" data-bs-toggle="modal" data-bs-target="#user-detail-modal" className="btn btn-dark main--cards--card--card-body--more-info" onClick={onClickHandler}>more info</a>
         </div>
       </div>
     </div>
