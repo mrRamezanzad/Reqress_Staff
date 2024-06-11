f4953dc2e139cdc62e90ff6f3d09ab2808997199
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-06-11 15:37:49 +0330

    feat(sort): add onChange for sort options

--- src/components/sort/Sort.tsx
+++ src/components/sort/Sort.tsx
@@ -1,8 +1,19 @@
-export default function Sort() {
+import { ChangeEvent } from 'react'
+
+export interface SortProps {
+  onChange: Function;
+}
+
+export default function Sort({ onChange }: SortProps) {
+  const onChangeHandler = (event: ChangeEvent<HTMLSelectElement>) => {
+    const value = event.currentTarget.value;
+    onChange(value);
+  }
+
   return (
-    <select name="select" id="select-box" className=" align-self-start rounded bg-dark text-white mb-2">
-      <option value="">Sort</option>
-      <option value="">Latest</option>
+    <select name="select" id="select-box" className=" align-self-start rounded bg-dark text-white mb-2" onChange={onChangeHandler}>
+      <option value="id">Sort</option>
+      <option value="id">Latest</option>
       <option value="id">Id</option>
       <option value="first_name">First Name</option>
       <option value="last_name">Last Name</option>
